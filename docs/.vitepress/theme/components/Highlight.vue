3aa0c1ad7301ed46a35474ddce73d5d5c3674773
roy <1592414780@qq.com>
2022-10-20 12:06:09 +0800

    .

---
+++ docs/.vitepress/theme/components/Highlight.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+const props = withDefaults(
+    defineProps<{
+        text?: string,
+        type?: 'pink' | 'tip' | 'error' | 'info'
+    }>(), {
+    text: '',
+    type: 'pink',
+}
+)
+</script>
+<template>
+    <span class='highlight' :class="[ `highlight-type-${props.type}` ]">
+        <slot>{{ props.text }}</slot>
+    </span>
+</template>
+
+<style scoped>
+.highlight {
+    border-radius: 4px;
+    padding-left: 0.2rem;
+    padding-right: 0.2rem;
+}
+
+.highlight-type-pink {
+    background: #fddcf7;
+}
+</style>
